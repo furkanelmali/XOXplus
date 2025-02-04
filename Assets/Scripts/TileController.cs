137d62082fcbac9db4a881f603aeeb98214cb5eb
Furkan Elmalı <46634314+furkanelmali@users.noreply.github.com>
2025-02-04 13:55:46 +0300

    v1.0
    Project created.

---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,12 @@
+                                    
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        Debug.Log("Tile clicked");
+    }
+}
