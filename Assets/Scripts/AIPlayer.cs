57349584843e61543bd4b642b09a793263a69e80
Furkan Elmalı <46634314+furkanelmali@users.noreply.github.com>
2025-02-16 17:44:05 +0300

    v1.5
    AI Player improved. Now AI firstly looking for winning move next looking for blocking move and then if these both not exists, makes a random move

--- Assets/Scripts/AIPlayer.cs
+++ Assets/Scripts/AIPlayer.cs
@@ -11,6 +11,77 @@ public class AIPlayer : MonoBehaviour
      int oNumber;
     public void MakeMove(TileState state, List<TileController> listTileController)
     {
+
+        var winMove =CheckNextWinningMove(listTileController,state);
+        if(winMove.Item1)
+        {
+            GameManager.Instance.oCountt++;
+            oNumber = GameManager.Instance.oCountt;
+
+            winMove.Item2.SetState(state,oNumber);
+            winMove.Item2.oNumber = oNumber;
+            return;
+        }
+        var blockMove =CheckNextWinningMove(listTileController,TileState.X);
+        if(blockMove.Item1)
+        {
+            GameManager.Instance.oCountt++;
+            oNumber = GameManager.Instance.oCountt;
+
+            blockMove.Item2.SetState(state,oNumber);
+            blockMove.Item2.oNumber = oNumber;
+            return;
+        }
+
+        
+        if(!blockMove.Item1)
+        {
+             GameManager.Instance.oCountt++;
+             oNumber = GameManager.Instance.oCountt;
+
+            blockMove.Item2.SetState(state,oNumber);
+            blockMove.Item2.oNumber = oNumber;
+            return;
+        }
+       
+
+    }
+
+    
+
+    public (bool,TileController) CheckNextWinningMove(List<TileController> listTileController, TileState state)
+    {
+        foreach (var tile in listTileController)
+        {
+            if (tile.MyState != TileState.Empty) continue;
+            
+            TileState originalState = tile.MyState;
+            tile.MyState = state;
+            
+            foreach (var direction in Enum.GetValues(typeof(Direction)))
+            {
+                var next = tile.GetNextTile((Direction)direction);
+                if (!next) continue;
+                
+                if (next.MyState != state) continue;
+                
+                var last = next.GetNextTile((Direction)direction);
+                if (!last) continue;
+                
+                if (last.MyState != state) continue;
+                
+                tile.MyState = originalState;
+                return (true,tile);
+            }
+            
+            tile.MyState = originalState;
+        }
+        
+        return (false,GetEmptyCells(listTileController,state));
+    }
+
+    public TileController GetEmptyCells(List<TileController> listTileController, TileState state)
+    {
         List<TileController> emptyCells = new List<TileController>();
        
         
@@ -24,16 +95,8 @@ public class AIPlayer : MonoBehaviour
 
             
         }
-        if (emptyCells.Count > 0)
-            { 
-                GameManager.Instance.oCountt++;
-                oNumber = GameManager.Instance.oCountt;
-                int randomIndex = UnityEngine.Random.Range(0, emptyCells.Count);
-                emptyCells[randomIndex].oNumber = oNumber;
-                
-                
-                emptyCells[randomIndex].SetState(state,oNumber);
-            }
-       
+
+        int randomIndex = UnityEngine.Random.Range(0, emptyCells.Count);
+        return emptyCells[randomIndex];
     }
 }
